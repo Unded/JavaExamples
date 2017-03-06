704aa1d0a5b392fac3bf87f193cf8a80f0eae34e
nnm <setoswow@yandex.ru>
2017-03-06 16:02:32 +0300

    Rebuild project

--- src/concurrency/executor/simple_example/SimpleDemo.java
+++ src/concurrency/executor/simple_example/SimpleDemo.java
@@ -13,25 +13,42 @@ public class SimpleDemo {
         int availableThreads = availableProcessors * 4;
         ExecutorService executorService = Executors.newFixedThreadPool(availableThreads);
 
+        Future future = executorService.submit(new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                int i = 12;
+                int result = i * 12;
+                if (true) throw new RuntimeException();
+                return result;
+            }
+        });
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
         // Print some info
         System.out.println("Available processors: " + availableProcessors);
         System.out.println("Available threads: " + availableThreads);
         System.out.println("Running threads");
 
-        executorService.execute(new ExecutorThread(latch1, "A"));
-        executorService.execute(new ExecutorThread(latch2, "B"));
-        executorService.execute(new ExecutorThread(latch3, "C"));
-        executorService.execute(new ExecutorThread(latch4, "D"));
-        try {
-            //Если создать только одну переменную CountDownLatch
-            //то главный поток закончится раньше созданных
-            latch1.await();
-            latch2.await();
-            latch3.await();
-            latch4.await();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+//        executorService.execute(new ExecutorThread(latch1, "A"));
+//        executorService.execute(new ExecutorThread(latch2, "B"));
+//        executorService.execute(new ExecutorThread(latch3, "C"));
+//        executorService.execute(new ExecutorThread(latch4, "D"));
+//        try {
+//            //Если создать только одну переменную CountDownLatch
+//            //то главный поток закончится раньше созданных
+//            latch1.await();
+//            latch2.await();
+//            latch3.await();
+//            latch4.await();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
         executorService.shutdown();
         System.out.println("Finishing threads");
